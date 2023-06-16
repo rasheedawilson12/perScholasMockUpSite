5ed67424a10819ee08ab93ca632f7e94428b4c5f
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-16 07:48:43 -0500

    Media query respsonsiveness added

---
+++ app.js
@@ -0,0 +1,8 @@
+// Global Variables
+
+// Hide mobile navbar
+const mobileNavbar = () => {
+  let mobileNavbar = document.querySelector(".mobileNavbar");
+
+  mobileNavbar.classList.toggle("hide");
+};
